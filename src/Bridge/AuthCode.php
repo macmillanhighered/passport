5621e75fcbf69cbc9e6ed78f0a240385e3e81986
John Razmus <john@razmus.net>
2019-06-11 21:05:16 -0500

    Update AuthCode.php

--- src/Bridge/AuthCode.php
+++ src/Bridge/AuthCode.php
@@ -6,8 +6,18 @@ use League\OAuth2\Server\Entities\Traits\EntityTrait;
 use League\OAuth2\Server\Entities\Traits\AuthCodeTrait;
 use League\OAuth2\Server\Entities\AuthCodeEntityInterface;
 use League\OAuth2\Server\Entities\Traits\TokenEntityTrait;
+use App\Ship\Helpers\BinHelper;
 
 class AuthCode implements AuthCodeEntityInterface
 {
     use AuthCodeTrait, EntityTrait, TokenEntityTrait;
+    
+    public function setUserIdentifier($identifier)
+    {
+        parent::setUserIdentifier(BinHelper::encodeUuid($identifier));
+    }
+    public function getUserIdentifier()
+    {
+        return BinHelper::decodeUuid($this->userIdentifier);
+    }
 }
